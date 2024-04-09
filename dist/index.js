15175e96512955e91cecb088fad3498679efe595
Dev-WaiYan <htetwaiwilsonsprite@gmail.com>
2024-04-09 10:21:37 +0630

    Stop tracking dist directory

--- dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.checkStrengthStatus = exports.generate = void 0;
-const generator_1 = require("./utils/generator");
-Object.defineProperty(exports, "generate", { enumerable: true, get: function () { return generator_1.generate; } });
-const validator_1 = require("./utils/validator");
-Object.defineProperty(exports, "checkStrengthStatus", { enumerable: true, get: function () { return validator_1.checkStrengthStatus; } });
